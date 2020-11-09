f6b0ae0a9cf57fff15557b0136af09f034606471
Yzwer <y1113292776@gmail.com>
2020-11-09 15:12:12 +0800

    Update c_bufferoverflow_case32.c

--- c_bufferoverflow_case32.c
+++ c_bufferoverflow_case32.c
@@ -1,1 +1,39 @@
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "securec_def.h"
+
+
+
+#define HEADER_LEN (20)
+
+int main(int argc, char **argv)
+{
+    unsigned char* pHeader = argv[1];
+    unsigned int Size = strlen(argv[1]);
+      
+    unsigned char uType = 0;
+    unsigned char uLen = 0;
+    unsigned char uTotalLen = 0;
+    unsigned char* pData = NULL;
+
+    pData = pHeader + HEADER_LEN;
+    uType = *(pData-1);
+
+    while (0 != uType)
+    {
+        uLen = (*pData) << 2;
+        uTotalLen += uLen; //Integer overflow
+        if (uTotalLen > 20 || 0 == uLen)
+        {
+            return;
+        }
+        
+        /* POTENTIAL FLAW: Pointer offset comes from external input, and reading after offset leads to out-of-bounds read.*/
+        pData += uLen;
+        uType = *(pData-1);// OUT-OF-BOUNDS READ
+    }
+}
+
