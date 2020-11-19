616f1e49776436e8473bd3e59dfcc3be77f09501
Yzwer <y1113292776@gmail.com>
2020-11-19 16:49:21 +0800

    strlen(src) - 1

---
+++ cwe681_c_strlen_calc_case01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/types.h>
+
+/*
+4、strlen计算结果用于整数运算，存在整数溢出
+*/
+void test04_c(char* src)
+{
+	unsigned char caTmpCode[256] = { 0 };
+
+	size_t len = strlen(src) - 1;   //error
+
+									/* POTENTIAL FLAW: strlen计算结果用于整数运算，存在整数溢出， */
+	memcpy_s(caTmpCode, sizeof(caTmpCode), src + 1, strlen(src) - 1);   //error
+
+	if (strlen(src) > 1)
+		len = strlen(src) - 1;      //ok
+
+}
+
+
+int main()
+{
+	char * p = "aa";
+	test04_c(p, strlen(p));
+	return 0;
+}
